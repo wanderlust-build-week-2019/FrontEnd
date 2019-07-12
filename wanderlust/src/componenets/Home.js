6462eca9a6a543201389510687943952912233ad
Robert Pedersen <rob84044@users.noreply.github.com>
2019-07-11 21:08:09 -0600

    Added components folder

---
+++ wanderlust/src/componenets/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  constructor() {
+    super();
+    state = {};
+  }
+  render() {
+    return (
+      <div>
+        <div />
+      </div>
+    );
+  }
+}
+
+export default Home;
